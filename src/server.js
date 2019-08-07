42c244db4c464fa483f76a094b03e2024ff5cac9
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-08-07 00:38:37 -0300

    boilerplate backend

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const routes = require("./routes");
+
+const server = express();
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3000, () => {
+  console.log("server is running");
+});
